1c809e1c91de9d694127425917368b8adf7b2e3c
uma-dev <22565959+uma-dev@users.noreply.github.com>
2024-05-10 12:03:34 -0600

    add(test): Added test

--- flyweight/src/test/java/com/umadev/flyweight/FlyweightApplicationTests.java
+++ flyweight/src/test/java/com/umadev/flyweight/FlyweightApplicationTests.java
@@ -1,13 +1,24 @@
 package com.umadev.flyweight;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.umadev.flyweight.model.Tree;
+import com.umadev.flyweight.service.TreeFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FlyweightApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void verifyNumberOfInstancesInFactoryAreCorrect() {
 
+    for (int i = 0; i < 500000; i++) {
+      Tree greenTree = TreeFactory.getTree("Ornamental");
+      Tree redTree = TreeFactory.getTree("Frutal");
+    }
+
+    assertEquals(500000, TreeFactory.getGreenCounter());
+    assertEquals(500000, TreeFactory.getRedCounter());
+  }
 }
